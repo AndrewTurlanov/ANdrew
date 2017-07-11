fa6d55956dcf959eaa976c77eca8ca5c2b424743
AndrewTurlanov <turlanov@mail.ru>
2017-07-11 23:53:13 +0300

    Create task_1.3.5.2.cs

---
+++ task_1.3.5.2.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson1
+{
+    class Program
+    {      
+
+        static void Main(string[] args)
+        {
+            
+            string message = "Результат: " + (8 * 2 + 5);
+            Console.WriteLine(message);
+
+        }
+
+
+    }
+}
