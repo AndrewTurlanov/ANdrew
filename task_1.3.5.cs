7267e6e3e57fa4568f4791b75b4767ed6dccd824
AndrewTurlanov <turlanov@mail.ru>
2017-07-11 23:47:01 +0300

    Create task_1.3.5.cs

---
+++ task_1.3.5.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson1
+{
+    class Program
+    {
+
+       
+
+        static void Main(string[] args)
+        {
+           
+             byte variable_1 = 1;
+             sbyte blom = -100;
+             int a = 10;
+             long wep = 9999999;
+             short q = 5000;
+             decimal y = -79808000;            
+             string _url = "http://www.yandex.ru";
+             bool isOpen = false;
+             var t = 150;
+             var t1 = true;
+           
+
+
+
+
+             variable_1 = 1 + 1;
+             blom = --blom;
+             a = ++a;
+             wep = 500000 + 50000;
+             q = 9000 - 500;
+             y = 79898989898 + 564354545364;
+             _url = "new site:" + " http://www.google.com";
+             isOpen = !isOpen;
+
+             t = 150 * 2;
+             t1 = true || false;
+            
+
+
+        }
+
+
+    }
+}
