0c4ad824d21e14eb2f973ac02c47ffbc387a64f7
AndrewTurlanov <turlanov@mail.ru>
2017-07-11 23:47:57 +0300

    Create task_1.3.5.1.cs

---
+++ task_1.3.5.1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson1
+{
+    class Program
+    {
+
+        static int f = 80;
+
+        static void Main(string[] args)
+        {
+            
+             int a = 10;                                 //10
+             int b = 15 + 25;                            //40
+             int c = a * b + 8;                          //408
+             int d = (5 + 5) / 2;                        //5
+             bool b_a = true;                            //true
+             bool b_b = true && false;                   //false
+             bool b_c = false || true;                   // true    
+             bool b_d = true != false;                   //true
+             bool b_e = b_a == b_c;                      // true
+             string s = "test" + " " + "test2";          //test test2   
+
+            
+        }
+
+
+    }
+}
